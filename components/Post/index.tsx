07ebcc6cd820c0c7ca6f5aa0f4ade491e5d16875
Dmitriy <d.efremov@sftpro.ru>
2022-03-22 20:01:21 +0300

    Added comment

--- components/Post/index.tsx
+++ components/Post/index.tsx
@@ -1,20 +1,18 @@
+import Link from 'next/link';
+
 import {IconButton, Paper, Typography} from "@material-ui/core";
 import Image from "next/image";
 
 import styles from './Post.module.scss';
-
-import {
-  ModeCommentOutlined as CommentsIcon,
-  Cached as RefreshIcon,
-  BookmarkBorderOutlined as FavoriteIcon,
-  ShareOutlined as ShareIcon
-} from '@material-ui/icons'
+import {PostActions} from "../PostActions";
 
 export const Post = () => {
   return (
     <Paper elevation={0} className='p-20' classes={{ root: styles.paper}}>
       <Typography variant='h5' className={styles.title}>
-        Британские инженеры представили робота с «жестами и мимикой человека» — это восхитило и напугало людей в сети
+        <Link href='/news/test-123'>
+          Британские инженеры представили робота с «жестами и мимикой человека» — это восхитило и напугало людей в сети
+        </Link>
       </Typography>
       <Typography className='mt-10 mb-15'>
         «Самый передовой человекоподобный робот» Ameca пытается нравиться людям, но некоторых пугает естественность его движений.
@@ -24,28 +22,7 @@ export const Post = () => {
         height={400}
         width={600}
       />
-      <ul className={styles.actions}>
-        <li>
-          <IconButton>
-            <CommentsIcon />
-          </IconButton>
-        </li>
-        <li>
-          <IconButton>
-            <RefreshIcon />
-          </IconButton>
-        </li>
-        <li>
-          <IconButton>
-            <FavoriteIcon />
-          </IconButton>
-        </li>
-        <li>
-          <IconButton>
-            <ShareIcon />
-          </IconButton>
-        </li>
-      </ul>
+      <PostActions />
     </Paper> 
   )
 }
